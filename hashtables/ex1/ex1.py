f117aa5cd085c375899eed26cbdd7a44b1033da9
Zealll <eriznis@gmail.com>
2020-02-14 12:44:13 -0500

    Finished Hashtables

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -9,11 +9,18 @@ from hashtables import (HashTable,
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+    for i in range(length):
+        val = hash_table_retrieve(ht, (limit - weights[i]))
+        hash_table_insert(ht, weights[i], i)
+        if val is not None:
+            if i > val:
+                return [i, val]
+            else:
+                # print([val, i])
+                return [val, i]
 
-    return None
+    # print(ht.storage)
+    # print(hash_table_retrieve(ht, i))
 
 
 def print_answer(answer):
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,15 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    hashtable = HashTable(length)
+    route = [None] * length
+    # print(tickets)
+    for tix in tickets:
+        hash_table_insert(hashtable, tix.source, tix.destination)
 
-    pass
+    start = "NONE"
+    for i in range(length):
+        route[i] = hash_table_retrieve(hashtable, start)
+        start = route[i]
+
+    return route[ :len(route) - 1] 
